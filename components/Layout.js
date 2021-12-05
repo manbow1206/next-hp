bf24cf8d4841c68c87b76acdd8e057ddf93137d0
manbow1206 <tyasunaga@iyell.jp>
2021-12-05 09:39:30 +0900

    [add]nav bar

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,26 @@ export default function Layout({ children, title = 'Hp by Next.js' }) {
         <title>{title}</title>
       </Head>
       <header>
-        <nav className='bg-gray-800 w-screen'></nav>
+        <nav className='bg-gray-800 w-screen'>
+          <div className='flex justify-center items-center leading-normal  pl-8 h-3'></div>
+          <div className='flex space-x-4'>
+            <Link href='/'>
+              <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
+                Home
+              </a>
+            </Link>
+            <Link href='/blog-page'>
+              <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
+                Blog
+              </a>
+            </Link>
+            <Link href='/contact-page'>
+              <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
+                Contact
+              </a>
+            </Link>
+          </div>
+        </nav>
       </header>
       <main className='flex flex-1 justify-center items-center flex-col w-screen'>
         {children}
