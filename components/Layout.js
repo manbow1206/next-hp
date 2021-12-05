0e3d62981c6dd2529d3c666d0eb204525d717c4a
manbow1206 <tyasunaga@iyell.jp>
2021-12-05 09:50:21 +0900

    [add]css

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Layout({ children, title = 'Hp by Next.js' }) {
   return (
@@ -32,6 +33,18 @@ export default function Layout({ children, title = 'Hp by Next.js' }) {
       <main className='flex flex-1 justify-center items-center flex-col w-screen'>
         {children}
       </main>
+      <footer className='w-full h-12 flex justify-center items-center border-t'>
+        <a
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Powered by
+          <span className="w-full h-12 justify-center items-center border-t">
+            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+          </span>
+        </a>
+      </footer>
     </div>
   );
 }
