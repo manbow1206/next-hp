b3a3398c420bc3c81fe75714c8631f308d7420ef
manbow1206 <tyasunaga@iyell.jp>
2021-12-05 11:03:33 +0900

    [add] blog-page

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,6 +1,21 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
+import Post from '../components/Post';
+import { getAllPostsData } from '../lib/posts';
 
-export const Blog = () => {
-  return <Layout title="Blog">Blog page</Layout>;
+export const Blog = ({ posts }) => {
+  return (
+    <Layout title='Blog'>
+      <ul className='m-10'>
+        {posts && posts.map((posts) => <Post key={posts.id} post={posts} />)}
+      </ul>
+    </Layout>
+  );
 };
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
