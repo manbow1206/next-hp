5561de10b2a5e171962573d035c0b25fea0a4d81
manbow1206 <tyasunaga@iyell.jp>
2021-12-05 11:48:17 +0900

    layout fix

--- components/Layout.js
+++ components/Layout.js
@@ -2,31 +2,32 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function Layout({ children, title = 'Hp by Next.js' }) {
+export default function Layout({ children, title = 'HP by Nextjs' }) {
   return (
-    <div className='flex justify-center items-center flex-col min-h-screen text-gray-400 text-sm font-mono'>
+    <div className='flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono'>
       <Head>
         <title>{title}</title>
       </Head>
       <header>
         <nav className='bg-gray-800 w-screen'>
-          <div className='flex justify-center items-center leading-normal  pl-8 h-3'></div>
-          <div className='flex space-x-4'>
-            <Link href='/'>
-              <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
-                Home
-              </a>
-            </Link>
-            <Link href='/blog-page'>
-              <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
-                Blog
-              </a>
-            </Link>
-            <Link href='/contact-page'>
-              <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
-                Contact
-              </a>
-            </Link>
+          <div className='flex items-center pl-8 h-14'>
+            <div className='flex space-x-4'>
+              <Link href='/'>
+                <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
+                  Home
+                </a>
+              </Link>
+              <Link href='/blog-page'>
+                <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
+                  Blog
+                </a>
+              </Link>
+              <Link href='/contact-page'>
+                <a className='text-gray-300 hover:bg-gray-700 px-3 py-2 rounded'>
+                  Contact
+                </a>
+              </Link>
+            </div>
           </div>
         </nav>
       </header>
@@ -35,15 +36,14 @@ export default function Layout({ children, title = 'Hp by Next.js' }) {
       </main>
       <footer className='w-full h-12 flex justify-center items-center border-t'>
         <a
-          className='flex items-center '
+          className='flex items-center'
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
           target='_blank'
           rel='noopener noreferrer'
         >
           Powered by{' '}
-          <span className='w-full h-12 justify-center items-center border-t'>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
+          {/* <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" /> */}
+          <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
         </a>
       </footer>
     </div>
